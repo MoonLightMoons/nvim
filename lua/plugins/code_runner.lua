5373316b309c4dccacf3ba6b05523783cebedda5
MoonLightMoon <a2757485565@163.com>
2024-11-30 21:34:14 +0800

    keymap and dap pre

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -2,5 +2,15 @@ return {
 	{
 		"CRAG666/code_runner.nvim",
 		config = true,
+		keys = {
+			{ "<leader>r", "<cmd>RunCode<cr>", desc = "RunCode" },
+		},
+		opts = {
+			mode = "float",
+			startinsert = true,
+			float = {
+				border = "rounded",
+			},
+		},
 	},
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,5 +3,12 @@ return {
 		"akinsho/toggleterm.nvim",
 		version = "*",
 		config = true,
+		opts = {
+			start_in_insert = true,
+			direction = "float",
+		},
+		keys = {
+			{ "<c-\\>", "<cmd>ToggleTerm<cr>", desc = "ToggleTerm" },
+		},
 	},
 }
